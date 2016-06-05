e912ece6d67a50598a2c1e53fe50942b3505902c
Harry Nelken <harry.nelken@gmail.com>
2016-06-05 15:25:23 -0400

    parsing is effective, database needs to be setup
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- src/java/src/OPTEDParser.java
+++ src/java/src/OPTEDParser.java
@@ -1,5 +1,8 @@
+import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
@@ -8,57 +11,29 @@ import org.ccil.cowan.tagsoup.jaxp.SAXParserImpl;
 public class OPTEDParser {
 
 	public static void main(String... args) throws Exception {
-		
-		// print the 'src' attributes of <img> tags
-	    // from http://www.yahoo.com/
-	    // using the TagSoup parser
 	
 		OPTEDParser opted = new OPTEDParser();
+	    LineHandler handler = opted.new LineHandler();
 	    SAXParserImpl parser = SAXParserImpl.newInstance(null);
 	    
+	    // Parse the dictionary
 	    parser.parse(
-	        new FileInputStream("dictionary filename here"),
-	        new DefaultHandler() {
-
-	        	String line;
-	        	StringBuilder builder;
-	        	
-	        	public void startElement(String uri, String localName,
-	                    String name, Attributes a) {
-	        		if (name.equalsIgnoreCase("p")) {
-	        			builder = new StringBuilder();
-	        		}
-	        	}
-	        	
-	        	public void characters(char[] ch, int start, int length) {
-	        		builder.append(new String(ch, start, length));
-	        	}
-
-	        	public void endElement(String uri, String localName, String name) {
-	        		if (name.equalsIgnoreCase("p")) {
-	        			line = builder.toString();
-
-	        			LineHandler handler = opted.new LineHandler(line);
-	        			InputStream stream = 
-	        			parser.parse()
-	        		}
-	        	}
-	        });
-    
+	        new FileInputStream("lib/opted/wb1913_a.html"),
+	        handler);
 	    
     }
 	
+	/**
+	 * A class for parsing the plain-text dictionary.
+	 * Parses each line for the word and definition and
+	 * commits these to the database, checking for duplicates.
+	 */
 	private class LineHandler extends DefaultHandler {
 	
-		private String line = null;
+		private int count = 100;
 		private String word = null;
 		private String definition = null;
-    	private boolean wordFound = false;
     	private StringBuilder builder;
-    	
-    	public LineHandler(String line) {
-    		this.line = line;
-    	}
     	
     	public String getWord() {
     		return word;
@@ -68,27 +43,44 @@ public class OPTEDParser {
     		return definition;
     	}
     	
-		public void startElement(String uri, String localName,
-                String name, Attributes a) {
-    		if (name.equalsIgnoreCase("b")) {
-    			builder = new StringBuilder();
+    	public void characters(char[] ch, int start, int length) {
+    		// append any characters to the string builder
+    		if (builder != null) {
+    			builder.append(new String(ch, start, length));
     		}
     	}
     	
-    	public void characters(char[] ch, int start, int length) {
-    		
-    		if (!wordFound) {
-    			// append any characters to the string builder
-    			builder.append(new String(ch, start, length));
-    		}
+		public void startElement(String uri, String localName,
+                String name, Attributes a) {
+			if (name.equalsIgnoreCase("b")) {
+				builder = new StringBuilder();
+			}
     	}
 
     	public void endElement(String uri, String localName, String name) {
     		if (name.equalsIgnoreCase("b")) {
     			word = builder.toString();
-    			wordFound = true;
+    			builder = new StringBuilder();
+    		}
+    		else if (name.equalsIgnoreCase("i")) {
+    			builder = new StringBuilder();
+    		}
+    		else if (name.equalsIgnoreCase("p")) {
+    			definition = builder.toString().substring(2);
+    			commitWord();
     		}
     	}
-		
+    	
+    	private void commitWord() {
+    		System.out.println("Word: " + word + "\nDefinition: " + definition + '\n');
+    		// Check DB if word is contained
+    		// If yes:
+    			// Add definition only
+    		// If no:
+    			// Add full new word entry
+    		if (count-- == 0) {
+    			System.exit(0);
+    		}
+    	}
 	}
 }
