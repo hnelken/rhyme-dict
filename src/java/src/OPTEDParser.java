66e63b4ff3787c80618b8600524d7b98dab92a9c
Harry Nelken <harry.nelken@gmail.com>
2016-06-06 18:48:10 -0400

    added DB connection to parser, working out kinks in queries
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

--- src/java/src/OPTEDParser.java
+++ src/java/src/OPTEDParser.java
@@ -1,8 +1,5 @@
-import java.io.ByteArrayInputStream;
 import java.io.FileInputStream;
-import java.io.InputStream;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
+import java.sql.*;
 
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
@@ -12,15 +9,33 @@ public class OPTEDParser {
 
 	public static void main(String... args) throws Exception {
 	
-		OPTEDParser opted = new OPTEDParser();
-	    LineHandler handler = opted.new LineHandler();
-	    SAXParserImpl parser = SAXParserImpl.newInstance(null);
-	    
-	    // Parse the dictionary
-	    parser.parse(
-	        new FileInputStream("lib/opted/wb1913_a.html"),
-	        handler);
-	    
+		Connection c = null;
+		Statement stmt = null;
+		
+	    try {
+	    	// Open the DB connection
+	    	Class.forName("org.sqlite.JDBC");
+	    	c = DriverManager.getConnection("jdbc:sqlite:dictionary.db");
+	    	System.out.println("DB Connection Successful");
+	    	
+	    	// Setup for parsing
+	    	OPTEDParser opted = new OPTEDParser();
+		    LineHandler handler = opted.new LineHandler(c);
+		    SAXParserImpl parser = SAXParserImpl.newInstance(null);
+		    
+		    // Parse the dictionary, making DB inserts as we go
+		    parser.parse(
+		        new FileInputStream("lib/opted/wb1913_z.html"),
+		        handler);
+	    	
+		    // Close the DB connection
+	    	c.close();
+	    	
+	    } catch ( Exception e ) {
+	    	c.close();
+	    	System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+	    	System.exit(0);	
+	    }
     }
 	
 	/**
@@ -30,10 +45,15 @@ public class OPTEDParser {
 	 */
 	private class LineHandler extends DefaultHandler {
 	
-		private int count = 100;
+		private int count = 500;
 		private String word = null;
 		private String definition = null;
     	private StringBuilder builder;
+    	private Connection c;
+    	
+    	public LineHandler(Connection conn) {
+    		this.c = conn;
+    	}
     	
     	public String getWord() {
     		return word;
@@ -72,14 +92,83 @@ public class OPTEDParser {
     	}
     	
     	private void commitWord() {
-    		System.out.println("Word: " + word + "\nDefinition: " + definition + '\n');
-    		// Check DB if word is contained
-    		// If yes:
-    			// Add definition only
-    		// If no:
-    			// Add full new word entry
-    		if (count-- == 0) {
-    			System.exit(0);
+    		
+    		System.out.println(
+    				"Word: " + word + 
+    				"\nDefinition: " + definition + '\n');
+    		
+    		int wordID = getWordID(word);
+    		if (wordID != -1) {
+	    		// Word exists, insert definition only
+    			insertWord(word);
+	    	}
+	    	else {
+	    		// Word doesn't exist, insert new word and its definition
+	    		insertDefinition(definition, wordID);
+	    	}
+    		
+    	}
+    	
+    	private void insertWord(String word) {
+    		try {
+	    		// Check DB if word is contained
+		    	Statement stmt = c.createStatement();
+		    	String sql = "INSERT INTO words(text) VALUES(text='" + word + "')";
+		    	
+		    	// Word exists if this query returns any results
+		    	stmt.executeUpdate(sql);
+		    	
+		    	// Close up this shop
+		    	stmt.close();
+		    	
+		    	// Insert definition
+		    	int wordID = getWordID(word);
+		    	insertDefinition(definition, wordID);
+    		}
+    		catch (Exception e) {
+    	    	System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+    		}
+    	}
+    	
+    	private void insertDefinition(String definition, int wordID) {
+    		try {
+	    		// Check DB if word is contained
+		    	Statement stmt = c.createStatement();
+		    	String sql = "INSERT INTO definitions(word_id, text) VALUES(word_id='" + wordID + "', text='" + word + "')";
+		    	
+		    	// Word exists if this query returns any results
+		    	stmt.executeUpdate(sql);
+		    	
+		    	// Close up shop
+		    	stmt.close();
+    		}
+    		catch (Exception e) {
+    	    	System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+    		}
+    	}
+    	
+    	private int getWordID(String word) {
+			// Return -1 if word doesn't exist
+			int wordID = -1;
+			
+    		try {
+	    		// Check DB if word is contained
+		    	Statement stmt = c.createStatement();
+		    	String sql = "SELECT id FROM words WHERE text='" + word + "'";
+		    	
+		    	// Word exists if this query returns any results
+		    	ResultSet results = stmt.executeQuery(sql);
+		    	if (results.next()) {
+		    		wordID = results.getInt("id");
+		    	}
+		    	
+		    	// Close up shop
+		    	results.close();
+		    	stmt.close();
+		    	return wordID;
+    		}
+    		catch (Exception e) {
+    			return wordID;
     		}
     	}
 	}
