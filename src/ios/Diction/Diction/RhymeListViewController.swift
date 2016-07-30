460233e02765d2f51ae615a1c37d4ffa38503832
Harry Nelken <harry.nelken@gmail.com>
2016-07-30 11:53:19 -0400

    added landing screen with search bar
    Signed-off-by: Harry Nelken <harry.nelken@gmail.com>

---
+++ src/ios/Diction/Diction/RhymeListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RhymeListViewController.swift
+//  Diction
+//
+//  Created by Harry Nelken on 7/30/16.
+//
+//
+
+import UIKit
+
+class RhymeListViewController: UIViewController {
+
+    var rhymes = ["HI"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ src/ios/Diction/Diction/SearchViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchViewController.swift
+//  Diction
+//
+//  Created by Harry Nelken on 7/30/16.
+//
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        navigationController?.navigationBarHidden = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
